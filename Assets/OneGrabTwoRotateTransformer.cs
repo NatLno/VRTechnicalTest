e2006a3c8c57f9c245e97bab3cf96d63a5213d18
Nathan Lenogue <82942062+NatLno@users.noreply.github.com>
2023-10-02 17:48:49 +0200

    feat: add joystick but not working

--- Assets/OneGrabTwoRotateTransformer.cs
+++ Assets/OneGrabTwoRotateTransformer.cs
@@ -3,9 +3,48 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class OneGrabTwoRotateTransformer : MonoBehaviour, ITransformer
 {
+    public enum JoystickType
+    {
+        BothCircle,
+        BothSquare,
+        FrontBack,
+        LeftRight,
+    }
+    
+    [SerializeField]
+    [Tooltip("The object that is visually grabbed and manipulated")]
+    Transform m_Handle = null;
+
+    [SerializeField]
+    [Tooltip("The value of the joystick")]
+    Vector2 m_Value = Vector2.zero;
+
+    [Tooltip("Controls how the joystick moves")]
+    [SerializeField]
+    JoystickType m_JoystickMotion = JoystickType.BothCircle;
+
+    [SerializeField]
+    [Tooltip("Maximum angle the joystick can move")]
+    [Range(1.0f, 90.0f)]
+    float m_MaxAngle = 60.0f;
+
+    [SerializeField]
+    [Tooltip("Minimum amount the joystick must move off the center to register changes")]
+    [Range(1.0f, 90.0f)]
+    float m_DeadZoneAngle = 10.0f;
+
+    [SerializeField]
+    [Tooltip("Events to trigger when the joystick's x value changes")]
+    UnityEvent m_OnValueChangeX = new UnityEvent();
+
+    [SerializeField]
+    [Tooltip("Events to trigger when the joystick's y value changes")]
+    UnityEvent m_OnValueChangeY = new UnityEvent();
+
     public enum Axis
     {
         Right = 0,
@@ -100,7 +139,6 @@ public class OneGrabTwoRotateTransformer : MonoBehaviour, ITransformer
 
         Vector3 localAxis = Vector3.zero;
         localAxis[(int)_rotationAxis1] = 1f;
-        localAxis[(int)_rotationAxis2] = 1f;
 
         _worldPivotPose = ComputeWorldPivotPose();
         Vector3 rotationAxis = _worldPivotPose.rotation * localAxis;
@@ -113,7 +151,6 @@ public class OneGrabTwoRotateTransformer : MonoBehaviour, ITransformer
         {
             Vector3 localAxisNext = Vector3.zero;
             localAxisNext[((int)_rotationAxis1 + 1) % 3] = 0.001f;
-            localAxisNext[((int)_rotationAxis2 + 1) % 3] = 0.001f;
             grabDelta = _worldPivotPose.rotation * localAxisNext;
         }
 
@@ -137,56 +174,94 @@ public class OneGrabTwoRotateTransformer : MonoBehaviour, ITransformer
         _transformPoseInPivotSpace.position /= parentScale;
     }
 
+    Vector3 GetLookDirection()
+    {
+        //Vector3 direction = m_Interactor.GetAttachTransform(this).position - m_Handle.position;
+        //direction = transform.InverseTransformDirection(direction);
+        //switch (m_JoystickMotion)
+        //{
+        //    case JoystickType.FrontBack:
+        //        direction.x = 0;
+        //        break;
+        //    case JoystickType.LeftRight:
+        //        direction.z = 0;
+        //        break;
+        //}
+
+        //direction.y = Mathf.Clamp(direction.y, 0.01f, 1.0f);
+        //return direction.normalized;
+        return Vector3.zero;
+    }
+
     public void UpdateTransform()
     {
-        var grabPoint = _grabbable.GrabPoints[0];
-        var targetTransform = _grabbable.Transform;
+        var lookDirection = GetLookDirection();
 
-        Vector3 localAxis = Vector3.zero;
-        localAxis[(int)_rotationAxis1] = 1f;
-        localAxis[(int)_rotationAxis2] = 1f;
-        _worldPivotPose = ComputeWorldPivotPose();
-        Vector3 rotationAxis = _worldPivotPose.rotation * localAxis;
+        // Get up/down angle and left/right angle
+        var upDownAngle = Mathf.Atan2(lookDirection.z, lookDirection.y) * Mathf.Rad2Deg;
+        var leftRightAngle = Mathf.Atan2(lookDirection.x, lookDirection.y) * Mathf.Rad2Deg;
 
-        // Project our positional offsets onto a plane with normal equal to the rotation axis
-        Vector3 targetOffset = grabPoint.position - _worldPivotPose.position;
-        Vector3 targetVector = Vector3.ProjectOnPlane(targetOffset, rotationAxis);
+        // Extract signs
+        var signX = Mathf.Sign(leftRightAngle);
+        var signY = Mathf.Sign(upDownAngle);
 
-        Vector3 previousVectorInWorldSpace =
-            _worldPivotPose.rotation * _previousVectorInPivotSpace;
+        upDownAngle = Mathf.Abs(upDownAngle);
+        leftRightAngle = Mathf.Abs(leftRightAngle);
 
-        // update previous
-        _previousVectorInPivotSpace = Quaternion.Inverse(_worldPivotPose.rotation) * targetVector;
+        var stickValue = new Vector2(leftRightAngle, upDownAngle) * (1.0f / m_MaxAngle);
 
-        float signedAngle =
-            Vector3.SignedAngle(previousVectorInWorldSpace, targetVector, rotationAxis);
-
-        _relativeAngle += signedAngle;
-
-        _constrainedRelativeAngle = _relativeAngle;
-        if (Constraints.MinAngle.Constrain)
+        // Clamp the stick value between 0 and 1 when doing everything but circular stick motion
+        if (m_JoystickMotion != JoystickType.BothCircle)
         {
-            _constrainedRelativeAngle = Mathf.Max(_constrainedRelativeAngle, Constraints.MinAngle.Value);
+            stickValue.x = Mathf.Clamp01(stickValue.x);
+            stickValue.y = Mathf.Clamp01(stickValue.y);
         }
-        if (Constraints.MaxAngle.Constrain)
+        else
         {
-            _constrainedRelativeAngle = Mathf.Min(_constrainedRelativeAngle, Constraints.MaxAngle.Value);
+            // With circular motion, if the stick value is greater than 1, we normalize
+            // This way, an extremely strong value in one direction will influence the overall stick direction
+            if (stickValue.magnitude > 1.0f)
+            {
+                stickValue.Normalize();
+            }
         }
 
-        Quaternion deltaRotation = Quaternion.AngleAxis(_constrainedRelativeAngle - _startAngle, rotationAxis);
+        // Rebuild the angle values for visuals
+        leftRightAngle = stickValue.x * signX * m_MaxAngle;
+        upDownAngle = stickValue.y * signY * m_MaxAngle;
 
-        float parentScale = targetTransform.parent != null ? targetTransform.parent.lossyScale.x : 1f;
-        Pose transformDeltaInWorldSpace =
-            new Pose(
-                _worldPivotPose.rotation * (parentScale * _transformPoseInPivotSpace.position),
-                _worldPivotPose.rotation * _transformPoseInPivotSpace.rotation);
+        // Apply deadzone and sign back to the logical stick value
+        var deadZone = m_DeadZoneAngle / m_MaxAngle;
+        var aliveZone = (1.0f - deadZone);
+        stickValue.x = Mathf.Clamp01((stickValue.x - deadZone)) / aliveZone;
+        stickValue.y = Mathf.Clamp01((stickValue.y - deadZone)) / aliveZone;
 
-        Pose transformDeltaRotated = new Pose(
-            deltaRotation * transformDeltaInWorldSpace.position,
-            deltaRotation * transformDeltaInWorldSpace.rotation);
+        // Re-apply signs
+        stickValue.x *= signX;
+        stickValue.y *= signY;
 
-        targetTransform.position = _worldPivotPose.position + transformDeltaRotated.position;
-        targetTransform.rotation = transformDeltaRotated.rotation;
+        SetHandleAngle(new Vector2(leftRightAngle, upDownAngle));
+        SetValue(stickValue);
+    }
+
+    void SetValue(Vector2 value)
+    {
+        //m_Value = value;
+        //m_OnValueChangeX.Invoke(m_Value.x);
+        //m_OnValueChangeY.Invoke(m_Value.y);
+    }
+
+    void SetHandleAngle(Vector2 angles)
+    {
+        if (m_Handle == null)
+            return;
+
+        var xComp = Mathf.Tan(angles.x * Mathf.Deg2Rad);
+        var zComp = Mathf.Tan(angles.y * Mathf.Deg2Rad);
+        var largerComp = Mathf.Max(Mathf.Abs(xComp), Mathf.Abs(zComp));
+        var yComp = Mathf.Sqrt(1.0f - largerComp * largerComp);
+
+        m_Handle.up = (transform.up * yComp) + (transform.right * xComp) + (transform.forward * zComp);
     }
 
     public void EndTransform() { }
@@ -201,7 +276,6 @@ public class OneGrabTwoRotateTransformer : MonoBehaviour, ITransformer
     public void InjectOptionalRotationAxis(Axis rotationAxis)
     {
         _rotationAxis1 = rotationAxis;
-        _rotationAxis2 = rotationAxis;
     }
 
     public void InjectOptionalConstraints(OneGrabRotateConstraints constraints)
