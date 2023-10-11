17e78a8ed579a6c7c4c320b521d6ace6d48799a7
Nathan LENOGUE <nathan.lenogue@epita.fr>
2023-10-11 22:12:32 +0200

    feat: fix clawmachine

--- Assets/OVRSocket.cs
+++ Assets/OVRSocket.cs
@@ -3,21 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.XR.Content.Interaction;
 
 public class OVRSocket : MonoBehaviour
 {
     [SerializeField]
     string tagName;
 
+    [SerializeField]
+    UfoAbductionForce ufoAbductionForce;
+
+    bool hasObject = false;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag.Equals(tagName))
-            other.gameObject.GetComponent<SnapInteractable>().enabled = true;
+        if (other.tag.Equals(tagName) && ufoAbductionForce.enabled && !hasObject)
+        {
+            other.gameObject.GetComponent<SnapInteractor>().enabled = true;
+            hasObject = true;
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag.Equals(tagName))
-            other.gameObject.GetComponent<SnapInteractable>().enabled = false;
+        if (other.tag.Equals(tagName) && !ufoAbductionForce.enabled)
+        {
+            other.gameObject.GetComponent<SnapInteractor>().enabled = false;
+            hasObject = false;
+        }
     }
 }
